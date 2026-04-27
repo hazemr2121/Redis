5dbf70ab08d3de14aa8e96ea89b8be00aa6e14c6
hazemr2121 <ha.ahmed8996@gmail.com>
2026-04-27 07:32:59 +0300

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,8 +1,8 @@
 const net = require("net");
-
+const db = new Map();
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
-
+const nullString = `$-1\r\n`;
 // Uncomment the code below to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
@@ -10,9 +10,15 @@ const server = net.createServer((connection) => {
     const raw = RESPParser(data);
     if (raw[0].toLowerCase() == "ping") {
       connection.write(`+PONG\r\n`);
-    }
-    if (raw[0].toLowerCase() == "echo") {
+    } else if (raw[0].toLowerCase() == "echo") {
       connection.write(`$${raw[1].length}\r\n${raw[1]}\r\n`);
+    } else if (raw[0].toLowerCase() == "set") {
+      db.set(raw[1], raw[2]);
+      connection.write(`+OK\r\n`);
+    } else if (raw[0].toLowerCase() == "get") {
+      let result = db.get(raw[1]);
+      if (result) connection.write(`$${result.length}\r\n${result}\r\n`);
+      connection.write(`$-1\r\n`);
     }
   });
 });
