c742054675cdf8225b32a4ac02d8998ed59be3ae
hazemr2121 <ha.ahmed8996@gmail.com>
2026-04-27 22:39:11 +0300

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,49 +1,68 @@
-const net = require("net");
-const db = new Map();
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-const nullString = `$-1\r\n`;
-// Uncomment the code below to pass the first stage
-const server = net.createServer((connection) => {
-  // Handle connection
-  connection.on("data", (data) => {
-    const raw = RESPParser(data);
-    if (raw[0].toLowerCase() == "ping") {
-      connection.write(`+PONG\r\n`);
-    } else if (raw[0].toLowerCase() == "echo") {
-      connection.write(`$${raw[1].length}\r\n${raw[1]}\r\n`);
-    } else if (
-      raw[0].toLowerCase() == "set" &&
-      (raw[3]?.toLowerCase() == "ex" || raw[3]?.toLowerCase() == "px")
-    ) {
-      db.set(raw[1], raw[2]);
-      connection.write(`+OK\r\n`);
-      setTimeout(
-        () => {
-          db.delete(raw[1]);
-        },
-        raw[3].toLowerCase() == "px" ? raw[4] : raw[4] * 1000,
-      );
-    } else if (raw[0].toLowerCase() == "set") {
-      db.set(raw[1], raw[2]);
-      connection.write(`+OK\r\n`);
-    } else if (raw[0].toLowerCase() == "get") {
-      let result = db.get(raw[1]);
-      if (result) connection.write(`$${result.length}\r\n${result}\r\n`);
-      else {
-        connection.write(`$-1\r\n`);
-      }
+const net = require("node:net");
+const TERMINATOR = "\r\n";
+const NULL = "$-1";
+const cache = new Map();
+const ECHO = "echo";
+const PING = "ping";
+const SET = "set";
+const GET = "get";
+const string = (s) => `+${s}`;
+
+const ping = () => {
+  return [string("PONG")];
+};
+const echo = ([type, arg]) => {
+  return [`\$${arg.length}`, arg];
+};
+const set = (args) => {
+  const [_, key, __, val, ___, flag, ____, expiry] = args;
+  cache.set(key, {
+    val,
+    ...(!!expiry && flag?.toLowerCase() == "px"
+      ? { expiry: Date.now() + parseInt(expiry) }
+      : {}),
+  });
+  return [string("OK")];
+};
+const get = ([type, key]) => {
+  let resp = [NULL];
+  if (cache.has(key)) {
+    const v = cache.get(key);
+    if (!v.expiry || (v.expiry && v.expiry > Date.now())) {
+      const val = v.val;
+      resp = [`\$${val.length}`, val];
     }
+  }
+  return resp;
+};
+const parseBuffer = (buff) => {
+  const resp = buff.toString();
+  const [_, __, cmd, ...args] = resp.split(TERMINATOR);
+  switch (cmd.toLowerCase()) {
+    case PING:
+      return ping(args);
+    case ECHO:
+      return echo(args);
+    case SET:
+      return set(args);
+    case GET:
+      return get(args);
+  }
+};
+/**
+ *
+ * @param {Buffer} buff
+ * @returns
+ */
+const handleData = (buff) => {
+  const resp = parseBuffer(buff);
+  resp.push("");
+  return resp.join(TERMINATOR);
+};
+
+const server = net.createServer((conn) => {
+  conn.on("data", (bufferData) => {
+    conn.write(handleData(bufferData));
   });
 });
-
 server.listen(6379, "127.0.0.1");
-
-function RESPParser(str) {
-  let raw = str.toString();
-  raw = raw.split("\r\n");
-  raw = raw.filter(
-    (el) => !el.startsWith("$") && !el.startsWith("*") && el !== "",
-  );
-  return raw;
-}
---
+++ old.js
@@ -0,0 +1,47 @@
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+console.log("Logs from your program will appear here!");
+const nullString = `$-1\r\n`;
+// Uncomment the code below to pass the first stage
+const server = net.createServer((connection) => {
+  // Handle connection
+  connection.on("data", (data) => {
+    const raw = RESPParser(data);
+    if (raw[0].toLowerCase() == "ping") {
+      connection.write(`+PONG\r\n`);
+    } else if (raw[0].toLowerCase() == "echo") {
+      connection.write(`$${raw[1].length}\r\n${raw[1]}\r\n`);
+    } else if (
+      raw[0].toLowerCase() == "set" &&
+      (raw[3]?.toLowerCase() == "ex" || raw[3]?.toLowerCase() == "px")
+    ) {
+      db.set(raw[1], raw[2]);
+      connection.write(`+OK\r\n`);
+      setTimeout(
+        () => {
+          db.delete(raw[1]);
+        },
+        raw[3].toLowerCase() == "px" ? raw[4] : raw[4] * 1000,
+      );
+    } else if (raw[0].toLowerCase() == "set") {
+      db.set(raw[1], raw[2]);
+      connection.write(`+OK\r\n`);
+    } else if (raw[0].toLowerCase() == "get") {
+      let result = db.get(raw[1]);
+      if (result) connection.write(`$${result.length}\r\n${result}\r\n`);
+      else {
+        connection.write(`$-1\r\n`);
+      }
+    }
+  });
+});
+
+server.listen(6379, "127.0.0.1");
+
+function RESPParser(str) {
+  let raw = str.toString();
+  raw = raw.split("\r\n");
+  raw = raw.filter(
+    (el) => !el.startsWith("$") && !el.startsWith("*") && el !== "",
+  );
+  return raw;
+}
