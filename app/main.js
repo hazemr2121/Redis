94bee8338f8179176b3e3294ac4c3267e95bedfe
hazemr2121 <ha.ahmed8996@gmail.com>
2026-04-27 23:28:56 +0300

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -6,7 +6,9 @@ const ECHO = "echo";
 const PING = "ping";
 const SET = "set";
 const GET = "get";
+const RPUSH = "rpush";
 const string = (s) => `+${s}`;
+const integer = (n) => [`:${n}`];
 
 const ping = () => {
   return [string("PONG")];
@@ -35,6 +37,18 @@ const get = ([type, key]) => {
   }
   return resp;
 };
+const rpush = (args) => {
+  const [_, list, __, ...values] = args;
+  let finalValues = values.filter((el) => el !== "");
+  if (cache.has(list)) {
+    const v = cache.get(list);
+    v.push(...finalValues);
+    return integer(v.length);
+  } else {
+    cache.set(list, [...finalValues]);
+    return integer(finalValues.length);
+  }
+};
 const parseBuffer = (buff) => {
   const resp = buff.toString();
   const [_, __, cmd, ...args] = resp.split(TERMINATOR);
@@ -47,6 +61,8 @@ const parseBuffer = (buff) => {
       return set(args);
     case GET:
       return get(args);
+    case RPUSH:
+      return rpush(args);
   }
 };
 /**
